b55672f51f990de4a6cd5fc96c25c070f8d16308
iacobo <47821203+iacobo@users.noreply.github.com>
2025-03-14 14:52:51 +0800

    remove extended icons

--- app/src/main/java/com.iacobo.wuziqi/ui/GameScreen.kt
+++ app/src/main/java/com.iacobo.wuziqi/ui/GameScreen.kt
@@ -3,22 +3,22 @@ package com.iacobo.wuziqi.ui
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
-import com.iacobo.wuziqi.data.GameState
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.iacobo.wuziqi.data.GameState
 
 @Composable
 fun GameScreen() {
