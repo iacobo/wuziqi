d729374768426bf7cdbcd979d307e462b6fc7401
iacobo <47821203+iacobo@users.noreply.github.com>
2025-03-16 04:11:29 +0800

    missing imports

--- app/src/main/java/com.iacobo.wuziqi/ui/GameScreen.kt
+++ app/src/main/java/com.iacobo.wuziqi/ui/GameScreen.kt
@@ -1,6 +1,7 @@
 package com.iacobo.wuziqi.ui
 
 import androidx.compose.animation.core.*
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -29,8 +30,11 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
