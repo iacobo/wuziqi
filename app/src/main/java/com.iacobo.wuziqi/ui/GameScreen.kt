8c7d493dccb09a357c5a3e231314bdaba440e712
iacobo <47821203+iacobo@users.noreply.github.com>
2025-03-30 05:56:28 +0800

    Add missing imports.

--- app/src/main/java/com.iacobo.wuziqi/ui/GameScreen.kt
+++ app/src/main/java/com.iacobo.wuziqi/ui/GameScreen.kt
@@ -34,6 +34,10 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
