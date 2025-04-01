037aa234d9961df9cec5b37a643da0596625a104
iacobo <47821203+iacobo@users.noreply.github.com>
2025-04-01 23:33:43 +0800

    Add missing import.

--- app/src/main/java/com.iacobo.wuziqi/ui/layout/GameLayoutScaffold.kt
+++ app/src/main/java/com.iacobo.wuziqi/ui/layout/GameLayoutScaffold.kt
@@ -1,6 +1,7 @@
 package com.iacobo.wuziqi.ui.layout
 
 import android.view.Surface
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
