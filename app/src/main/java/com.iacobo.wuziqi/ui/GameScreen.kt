a8f886a50fd38f2ef8dd8c54425e696c4574b98d
iacobo <47821203+iacobo@users.noreply.github.com>
2025-04-01 21:00:59 +0800

    Add missing imports (again, merge conflicts.......)

--- app/src/main/java/com.iacobo.wuziqi/ui/GameScreen.kt
+++ app/src/main/java/com.iacobo.wuziqi/ui/GameScreen.kt
@@ -1,6 +1,11 @@
 package com.iacobo.wuziqi.ui
 
 import android.content.res.Configuration
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -9,6 +14,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import com.iacobo.wuziqi.R
 import com.iacobo.wuziqi.data.GameType
 import com.iacobo.wuziqi.data.ThemeMode
 import com.iacobo.wuziqi.ui.board.GameBoardRenderer
